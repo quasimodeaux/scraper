a76c826377c34153d807abf712d780e650215d5e
K <gatchey@yahoo.com>
2018-01-28 16:52:03 -0500

    server change

--- server.js
+++ server.js
@@ -1,51 +1,85 @@
-const cheerio = require("cheerio");
-const request = require("request");
-const express = require("express");
-const mongojs = require("mongojs");
+// Include Server Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+const logger = require("morgan");
+const bluebird = require('bluebird');
+var mongoose = require("mongoose");
 
-var db = mongojs('spaceStuff');
-var myCollection = db.collection('spaceCollection');
+// Require schema
+const Article = require("./models/Article");
 
-const $ = require("cheerio");
+// Create a new express app
+var app = express();
+// Sets an initial port. We'll use this later in our listener
+var PORT = process.env.PORT || 3000;
 
-var url = "https://www.reddit.com/r/woahdude/";
+// Run Morgan for Logging
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-request(url, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // Print the HTML for the Google homepage.
+app.use(express.static("public"));
 
-    var $ = cheerio.load(body);
+// -------------------------------------------------
 
+// MongoDB configuration (Change this URL to your own DB)
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/mydatabase");
+var db = mongoose.connection;
 
-    $('p.title').each( function(i, element){
-        var title = $(element).children('a').attr("title", "rel title").text();
-        console.log(title)
-        var text = $(element).children('a').attr("href");
-        console.log(text)
+db.on("error", function(err) {
+    console.log("Mongoose Error: ", err);
+});
 
-        if (text != null){
-            db.spaceStuff.insert({
-                    title: text
-                }, function(err, data){
-                    if (err){
-                        console.log(err)
-                    }
-                    else{
-                        console.log(data)
-                    }
-                }
-            )
+db.once("open", function() {
+    console.log("Mongoose connection successful.");
+});
+
+// -------------------------------------------------
+app.get("/api/saved", function(req, res) {
+    Article.find({}, function(error, doc) {
+        if(error) {
+            console.log(error)
+        } else {
+            res.json(doc)
         }
+    });
+});
 
-    })
+app.post("/api/saved", function(req, res) {
+    var newArticle = new Article({
+        title:req.body.title,
+        link:req.body.link,
+        date:req.body.date
+    });
+    newArticle.save(function(error, doc) {
+        if (error) {
+            console.log(error)
+        } else {
+            res.send(doc)
+        }
+    });
+});
 
+app.delete("/api/saved/:id", function(req, res) {
+    Article.find({_id: req.params.id}).remove(function(error, doc) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.send({result: "success"})}
+    });
+});
 
-
+app.get("*", function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 
+// -------------------------------------------------
 
-
-
-
+// Starting our express server
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
